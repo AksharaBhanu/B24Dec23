09b4774cfbe4efd8dd770783e3859c3436c6f4c6
BhanuQsp <javagalbhanu@gmail.com>
2021-12-25 08:53:49 +0530

    Updated with grid

--- src/test/java/generic/BaseTest.java
+++ src/test/java/generic/BaseTest.java
@@ -1,13 +1,20 @@
 package generic;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Reporter;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 import org.testng.asserts.SoftAssert;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -19,12 +26,19 @@ public class BaseTest implements IAutoConst{
 	static 
 	{
 		WebDriverManager.chromedriver().setup();
+		WebDriverManager.firefoxdriver().setup();
 	}
+	
+
 	@BeforeMethod(alwaysRun = true)
-	public void openApp() {
-		softAssert=new SoftAssert();
+	public void openApp() throws Exception {
+	
+//		driver=new ChromeDriver();
 		
-		driver=new ChromeDriver();
+		URL remoteURL=new URL("http://localhost:4444/wd/hub");
+		DesiredCapabilities capability=new DesiredCapabilities();
+		capability.setBrowserName("chrome");
+		driver=new RemoteWebDriver(remoteURL, capability);
 		
 		long ETO=Long.parseLong(Property.getProperty(PPT_PATH,"ETO"));
 		wait = new WebDriverWait(driver, Duration.ofSeconds(ETO));
@@ -34,10 +48,13 @@ public class BaseTest implements IAutoConst{
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(ITO));
 		
 		String App_URL=Property.getProperty(PPT_PATH,"URL");
-		driver.get(App_URL);
+		driver.get(App_URL);//if url is diff for each build
+//		driver.get(APP_URL); from interface when all Build has same URL
+		
 		
 		driver.manage().window().maximize();
-
+		
+		softAssert=new SoftAssert();
 	}
 	
 	@AfterMethod(alwaysRun = true)
--- src/test/java/generic/IAutoConst.java
+++ src/test/java/generic/IAutoConst.java
@@ -2,6 +2,6 @@ package generic;
 
 public interface IAutoConst {
 	String XL_PATH="./src/test/resources/data/input.xlsx";
-	String APP_URL="http://www.google.com";
+	String APP_URL="http://www.google.com";//Use this only when URL is same for all the builds
 	String PPT_PATH="./src/test/resources/config.properties";
 }
